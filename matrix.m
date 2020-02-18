af5d166d02f164a358351334e291fd3337817d2b
ignissen <jozsefpetrus99@gmail.com>
2020-02-18 15:15:50 +0100

    progress

--- matrix.m
+++ matrix.m
@@ -17,8 +17,42 @@ size(A) - kételemû sorvektor
 length - egy skalár -> max(size(A))
 többi függvény vektorok mintájára
 %}
+
 %mátrix -> vektor
 a(:);
+
 %sor/oszlopcsere
 a([1,2],:) = a([2,1],:);
 %oszlopcsere hasonlóan
+
+%beépített mátrixok
+eye(5); % 5x5-ös egységmátrix
+eye(2,4); %2x4-es egységmátrix
+ones(2); % egyesekbõl álló 2x2-es mátrix
+ones(2,4); % egyesekbõl álló 2x4-es mátrix
+%mátrixmûveletek
+%A+B
+%A-b
+%c*A
+%A^2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
